af4fb5810e2e3cb5728022156bccfbbc83d239d4
dina-03 <dingrafikweb@gmail.com>
2020-08-10 18:06:38 +0200

    lesson36-HW

--- src/lesson36/HomeWork36.java
+++ src/lesson36/HomeWork36.java
@@ -6,14 +6,14 @@ import java.util.Arrays;
 public class HomeWork36 {
     public static void main(String[] args) {
         System.out.println(cyrillicAlphabet());
-        System.out.println(intermingleAlphabet(cyrillicAlphabet()));
-
+        System.out.println(Arrays.toString(cyrillicAlphabet()));
+        System.out.println(Arrays.toString(intermingleAlphabet(cyrillicAlphabet())));
     }
 
     /*Объявите массив char и заполните его кириллическим алфавитом. */
     public static char[] cyrillicAlphabet() {
-        return new char[]{'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р',
-                'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'};
+        return new char[]{'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О',
+                'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'};
     }
 
     /* Перемешать его элементы случайным образом так, чтобы каждый элемент оказался на новом месте. ****/
